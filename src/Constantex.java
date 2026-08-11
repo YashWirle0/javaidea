038d3d228ddb6d4c452512f70464d7a5fea21e91
Yash Wirle <wirleyash0@gmail.com>
2026-08-11 23:11:43 +0530

    constants updated

---
+++ src/Constantex.java
@@ -0,0 +1,13 @@
+public class Constantex {
+    public static void main(String[] args){
+        //Constants
+        int age = 20;
+        age = 19;
+        age = 21;
+
+        final float age = 20.5F;
+        //cannot change the value of a final variable
+        age = 20;
+    }
+
+}
