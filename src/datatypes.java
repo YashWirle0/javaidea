00d1957fc7c8dca6627b43883aefc97c4b53fe12
Yash Wirle <wirleyash0@gmail.com>
2026-08-11 11:14:24 +0530

    Updated project code

--- src/datatypes.java
+++ src/datatypes.java
@@ -1,2 +1,24 @@
 public class datatypes {
+    public static void main(String[] args) {
+        //Primitive types
+        //byte - 1 [ -128 to 127 ]
+        //short - 2
+        //int - 4  1,2,3,4,5
+        //long - 8
+        //float - 4 3.14
+        //double - 8
+        //char - 2 a,b,c,d
+        //boolean - 1 true/false
+        byte age = 20;
+        int phone = 1234567890;
+        long phone2 = 1234567890L;
+        float pi = 3.14F;
+        char letter = 'a';
+        boolean isadult = true;
+
+        //Non-Primitive types
+        String name = "YASH";
+        String friend = new String ("sherrr");
+        System.out.println(name.length());
+    }
 }
