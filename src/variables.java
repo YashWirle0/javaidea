70f4aca3fd320c12ecd032e993aafceeb0ca8aa4
Yash Wirle <wirleyash0@gmail.com>
2026-08-11 10:40:45 +0530

    Initial commit

---
+++ src/variables.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class variables {
+    public static void main(String[] args) {
+        String name = "Yash";
+        var age = 20;
+        String surname = "wirle";
+        String fullname = name + " " + surname;
+        System.out.println(fullname);
+    }
+}
