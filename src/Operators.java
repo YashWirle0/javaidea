2c81a657f7603d9a1188bd5453d19ec7e7e69f55
Yash Wirle <wirleyash0@gmail.com>
2026-08-11 23:23:32 +0530

    operators updated

--- src/Operators.java
+++ src/Operators.java
@@ -10,7 +10,13 @@ public class Operators {
         //Division
         int c = 20;
         int d = 30;
-        int div = a / d;
+        int div = c / d;
         System.out.println(div);
+
+        //Division
+        double e = 20;
+        double f = 30;
+        double div1 = e / f;
+        System.out.println(div1);
     }
 }
