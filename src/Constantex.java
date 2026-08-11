cc56ad9e23dc76367c5c08502feaf8d6e35c3213
Yash Wirle <wirleyash0@gmail.com>
2026-08-11 23:21:03 +0530

    operators

--- src/Constantex.java
+++ src/Constantex.java
@@ -5,9 +5,9 @@ public class Constantex {
         age = 19;
         age = 21;
 
-        final float age = 20.5F;
+        final float age1 = 20.5F;
         //cannot change the value of a final variable
-        age = 20;
+
     }
 
 }
---
+++ src/Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+    public static void main(String[] args) {
+        //operators
+        //multiplication
+        int a = 20;
+        int b = 30;
+        int mul = a * b;
+        System.out.println(mul);
+
+        //Division
+        int c = 20;
+        int d = 30;
+        int div = a / d;
+        System.out.println(div);
+    }
+}
