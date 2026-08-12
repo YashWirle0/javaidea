0e441b04ccb9e2646b915c624a409d8a5b865a2c
Yash Wirle <wirleyash0@gmail.com>
2026-08-12 09:28:22 +0530

    Mathclass example

--- src/Operators.java
+++ src/Operators.java
@@ -18,5 +18,20 @@ public class Operators {
         double f = 30;
         double div1 = e / f;
         System.out.println(div1);
+
+        //modulo
+        double g = 20;
+        double h = 30;
+        double modulo = g % h;
+        System.out.println(modulo);
+
+        //assignment operator
+        int num = 1;
+        num++;
+        System.out.println(num);//2
+
+        int num1 = 5;
+        System.out.println(num++);//2
+        System.out.println(++num);//4
     }
 }
---
+++ src/mathex.java
@@ -0,0 +1,11 @@
+public class mathex {
+    public static void main(String[] agrs) {
+        //math
+        //max and min
+        System.out.println(Math.min(5, 6));
+        System.out.println(Math.max(5, 6));
+
+        //random
+        System.out.println(Math.random());
+    }
+}
