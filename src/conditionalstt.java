4781dbfa64f20fff462bca466e06f2b744770bee
Yash Wirle <wirleyash0@gmail.com>
2026-08-12 19:24:59 +0530

    Conditional operator

---
+++ src/conditionalstt.java
@@ -0,0 +1,15 @@
+public class conditionalstt {
+    public static void main(String[] args) {
+        boolean isSunUp = true;
+        if(isSunUp = true)
+            System.out.println("day");
+        else
+            System.out.println("night");
+
+        int age = 20;
+        if(age < 18)
+            System.out.println("You are not elibilbe for voting");
+        else
+            System.out.println("You are eligible for voting");
+    }
+}
